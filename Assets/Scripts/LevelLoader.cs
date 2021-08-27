f1bbf92b77a5b19734b389fdf325a6e981716205
LW <45142670+DevolpingWebsite@users.noreply.github.com>
2021-08-27 17:31:49 +0100

    crossfade added

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+	public void LoadNextLevel(int buildIndex)
+	{
+		StartCoroutine(LoadLevel(buildIndex));
+	}
+
+	public IEnumerator LoadLevel(int buildIndex)
+	{
+		transition.SetTrigger("Start");
+
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene(buildIndex);
+	}
+}
--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class Road : MonoBehaviour
 {
     [SerializeField] private Lane lane = Lane.Blue;
-	// Start is called before the first frame update
-	void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void OnMouseDown()
