50a2a14370d0b1922160f52182069c1a1c20973c
LW <45142670+DevolpingWebsite@users.noreply.github.com>
2021-08-28 13:42:51 +0100

    main menu has vroom vrooms

---
+++ Assets/MainMenuCar.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class MainMenuCar : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    public Car.CarType carType;
+    public Road.Lane currentLane;
+    private SpriteRenderer sr;
+    [SerializeField] private List<Sprite> carSprites = new List<Sprite>();
+    [SerializeField] private GameObject selectObject;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int index = (int)carType;
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = carSprites.ElementAt(index);
+        StartCoroutine(DestroyIn(6f));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int index = (int)carType;
+        sr.sprite = carSprites.ElementAt(index);
+        float realSpeed = speed / 1000;
+        if (carType == Car.CarType.Bomb || carType == Car.CarType.SlowArrow || carType == Car.CarType.SpeedArrrow)
+        {
+            realSpeed += 0.05f;
+        }
+
+        //Debug.Log("Raw Seconds: " + GameManager.instance.rawSeconds + " Speed Multiplier: " + GameManager.instance.speedMultiplier + " Change By: " + changeBy + " Divided Speed: " + speed);
+        transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + realSpeed);
+
+
+    }
+
+    private IEnumerator DestroyIn(float seconds)
+	{
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+	}
+}
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
+	private float secondsCount;
+	private float rawSeconds;
+	private int minutesCount;
+    private int lastSecond;
+    [SerializeField] private GameObject uiCarPrefab;
 
+	// Start is called before the first frame update
+	void Update()
+    {
+        StopWatch();
+        if((int)rawSeconds % 3 == 0 && lastSecond != (int)rawSeconds)
+		{
+            lastSecond = (int)rawSeconds;
+            SpawnUICar();
+            
+		}
     }
 
-    // Update is called once per frame
-    public void Play()
+	private void SpawnUICar()
+	{
+        Car.CarType carType = (Car.CarType)Random.Range(0, 3);
+        Road.Lane lane = (Road.Lane)Random.Range(0, 3);
+
+        float x = 0f;
+        float y = -9.1f;
+
+        switch (lane)
+        {
+            case Road.Lane.Red:
+                carType = Car.CarType.Red;
+                x = -9f;
+                break;
+            case Road.Lane.Purple:
+                carType = Car.CarType.Purple;
+                x = -0f;
+                break;
+            case Road.Lane.Blue:
+                carType = Car.CarType.Blue;
+                x = 9f;
+                break;
+        }
+
+        if ((int)carType > 3)
+        {
+            x = x + .75f;
+        }
+        else
+        {
+            x = x - .75f;
+        }
+
+        GameObject car = Instantiate(uiCarPrefab, new Vector2(x,y), Quaternion.identity);
+        car.GetComponent<MainMenuCar>().currentLane = lane;
+        car.GetComponent<MainMenuCar>().carType = carType;
+
+	}
+
+	// Update is called once per frame
+	public void Play()
     {
         SceneManager.LoadScene(1);
     }
@@ -27,4 +76,29 @@ public class MainMenuManager : MonoBehaviour
          Application.Quit();
 #endif
     }
+
+    public void StopWatch()
+    {
+        secondsCount += Time.deltaTime;
+        rawSeconds += Time.deltaTime;
+        string minutesText;
+        string secondsText;
+        Debug.Log(rawSeconds);
+        if (minutesCount < 10)
+            minutesText = "0" + minutesCount;
+        else
+            minutesText = minutesCount + "";
+        if ((int)secondsCount < 10)
+            secondsText = "0" + (int)secondsCount;
+        else
+            secondsText = (int)secondsCount + "";
+
+
+        if (secondsCount >= 60)
+        {
+            minutesCount++;
+            secondsCount = 0;
+        }
+    }
+
 }
